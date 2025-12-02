bfe340b59f6d99f68d4d6b182839216a6359cc6c
joseph-f-sato <joseph.f.toaster@gmail.com>
2025-12-02 20:16:17 +0900

    comment : ツリー描画の慣習に従い、フォルダを先、ファイルを後の順序で結合する。

--- src/tree_scanner.py
+++ src/tree_scanner.py
@@ -8,24 +8,27 @@ FILE_ICON = '📄'
 FOLDER_ICON = '📂'
 
 def _recursive_scan_dir(current_path, indent_prefix, current_tree_list):
+    #親フォルダ配下にあるすべてのフォルダ名・ファイル名をリスト化する。(フルパスではない)
     current_dir_file_list = os.listdir(current_path)
-
+    #フォルダとファイルを分離していれる空のリストを用意する。
     dirs = []
     files = []
-
+    
     for source in current_dir_file_list:
+        #親フォルダ配下にあるフォルダやファイルのフルパスをfull_pathに格納する。
         full_path = os.path.join(current_path,source)
 
-          # フォルダとファイルを分離
+        # フォルダとファイルを分離する。
         if os.path.isdir(full_path):
             dirs.append(source)
         else:
             files.append(source)
-
+    #ルール(五十音順、昇順など)に従いソートする。
     dirs.sort()
     files.sort()
+    #ツリー描画の慣習に従い、フォルダを先、ファイルを後の順序で結合する。
     all_children = dirs + files
-
+    #
     for i, item in enumerate(all_children):
         is_dir = item in dirs
         is_last = (i == len(all_children) -1)
